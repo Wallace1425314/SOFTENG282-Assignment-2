7586d52834a80dbf7cf4ad825ecd09f988694349
Wallace <wallace.wu98@gmail.com>
2024-04-18 16:35:55 +1200

    Code now takes input from the args passed into the command line.

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(findFibonacciSum(4));
-        System.out.println(findFibonacciSum(3));
-        System.out.println(findFibonacciSum(10));
-        System.out.println(findFibonacciSum(2));
-        System.out.println(findGcd(10, 15));
-        System.out.println(findGcd(14, 28));
-        System.out.println(findGcd(3, 9));
-        System.out.println(findGcd(4, 8));
-        System.out.println(findGcdOfTwoFibonacciSums(4, 3));
-        System.out.println(findGcdOfTwoFibonacciSums(10, 15));
-        System.out.println(findGcdOfTwoFibonacciSums(14, 28));
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+        findGcdOfTwoFibonacciSums(n, m);
     }
 
     public static int findFibonacciSum(int n) {
@@ -18,8 +10,10 @@ public class Main {
         int a = 0, b = 1, c = 0, sum = 1;
         // If n is less than or equal to 2 then return the corresponding fibbonaci sum
         if (n <= 0) {
+            System.out.println("The sum of the first " + n + " Fibonacci numbers is " + 0);
             return 0;
         } else if (n == 1) {
+            System.out.println("The sum of the first " + n + " Fibonacci numbers is " + 1);
             return 1;
         }
         // Calculate the sum of the Fibonacci sequence up to the nth number
@@ -29,12 +23,14 @@ public class Main {
             b = c;
             sum += c;
         }
+        System.out.println("The sum of the first " + n + " Fibonacci numbers is " + sum);
         return sum;
     }
 
     public static int findGcd(int a, int b) {
         // Find the GCD of two numbers by using the Euclidean algorithm
         if (b == 0) {
+            System.out.println("The GCD of the two numbers is " + a);
             return a;
         }
         return findGcd(b, a % b);
